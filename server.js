c9f47cd97e214e34b2548d6d1d5e696edd7d1a8e
GunaDD <gunadigan@gmail.com>
2024-07-12 11:02:46 +0700

    Localhost
    deploy localhost

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    
+    socket.on('locationUpdate', (data) => {
+        io.emit('locationUpdate', data); // Broadcast to all clients
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
