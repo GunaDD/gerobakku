edb0acdd5c1797d06fedbbf4dd2a5df010ec8888
GunaDD <gunadigan@gmail.com>
2024-07-12 12:17:56 +0700

    Online User

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ const io = socketIo(server);
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
-    console.log('New client connected');
+    console.log('New client connected:', socket.id);
     
     socket.on('locationUpdate', (data) => {
-        io.emit('locationUpdate', data); // Broadcast to all clients
+        io.emit('locationUpdate', { id: socket.id, latlng: data.latlng });
     });
 
     socket.on('disconnect', () => {
-        console.log('Client disconnected');
+        console.log('Client disconnected:', socket.id);
     });
 });
 
